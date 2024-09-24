cdd76dec35ebd2469eb1d854e1a5819e4b254dc4
Arthuremilio <arthur.ecf@hotmail.com>
2024-09-24 15:02:26 -0300

    FEAT: Create e READ dos pedidos no banco de dados

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-flutter-759e8-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-flutter-759e8-default-rtdb.firebaseio.com/orders';
 }
